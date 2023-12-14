735bbbc31cf9f47f599a9d6f5311eff9b4de09e0
ma10s <mattis.janitz@nav.no>
2023-12-14 13:13:09 +0100

    Fjerner logBesøk. Gjøres av dekoratøren.

--- src/amplitude/amplitude.ts
+++ src/amplitude/amplitude.ts
@@ -27,11 +27,3 @@ export const logNavigering = (
     applikasjon: 'ef-minside',
   });
 };
-
-export const logBesøk = (side: string) => {
-  logEvent('besøk', {
-    side,
-    team_id: 'familie',
-    applikasjon: 'familie-ef-ettersending',
-  });
-};
--- src/pages/Forside/Forside.tsx
+++ src/pages/Forside/Forside.tsx
@@ -13,7 +13,6 @@ import LenkePanelStorListe from './LenkePanelStorListe';
 import StønadPanelListe from './StønadPanelListe';
 import { GuidePanel } from '@navikt/ds-react';
 import { useEffect } from 'react';
-import { logBesøk } from '../../amplitude/amplitude';
 
 const HovedInnhold = styled(ResponsiveFlexbox)`
   background-color: ${ASurfaceActionSubtle};
@@ -38,10 +37,6 @@ const TittelContainer = styled.div`
 `;
 
 const Forside: React.FC = () => {
-  useEffect(() => {
-    logBesøk('Forside');
-  }, []);
-
   return (
     <main id="maincontent" tabIndex={-1} role="main">
       <ResponsiveFlexbox $justify="center" $padding="2.5rem">
