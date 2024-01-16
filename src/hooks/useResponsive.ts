76e2e80f5140599ba9e4c364a383b9ecae4cc44e
Viktor Solberg <viktor.grondalen.solberg@nav.no>
2024-01-16 12:53:12 +0100

    legger til hook useResponsive. Skal kunne endre props basert på skjermstørrelse

---
+++ src/hooks/useResponsive.ts
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+import { useDebouncedCallback } from './useDebounce';
+
+// Basert på https://learnersbucket.com/examples/interview/useresponsive-hook-in-react/
+const useResponsive = () => {
+  // screen resolutions
+  const [state, setState] = useState({
+    isMobile: false,
+    isTablet: false,
+    isDesktop: false,
+  });
+
+  useEffect(() => {
+    // update the state on the initial load
+    onResizeHandler();
+
+    // assign the event
+    Setup();
+
+    return () => {
+      // remove the event
+      Cleanup();
+    };
+  }, []);
+
+  // update the state on window resize
+  const onResizeHandler = () => {
+    const isMobile = window.innerWidth <= 768;
+    const isTablet = window.innerWidth >= 768 && window.innerWidth <= 990;
+    const isDesktop = window.innerWidth > 990;
+
+    setState({ isMobile, isTablet, isDesktop });
+  };
+
+  // debounce the resize call
+  const debouncedCall = useDebouncedCallback(onResizeHandler, 500);
+
+  // add event listener
+  const Setup = () => {
+    window.addEventListener('resize', debouncedCall, false);
+  };
+
+  // remove the listener
+  const Cleanup = () => {
+    window.removeEventListener('resize', debouncedCall, false);
+  };
+
+  return state;
+};
+
+export default useResponsive;
