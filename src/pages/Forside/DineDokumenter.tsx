4e465ce6f5ce4dfd53add5e955a8393bac3ba693
Viktor Solberg <viktor.grondalen.solberg@nav.no>
2024-01-19 13:36:04 +0100

    forenklet Forside.tsx, fjernet unødvendig kode

---
+++ src/pages/Forside/DineDokumenter.tsx
@@ -0,0 +1,30 @@
+import LenkePanelMikro from '../../components/LenkePanelMikro';
+import { useApp } from '../../context/AppContext';
+import { HStack, VStack } from '@navikt/ds-react';
+import { HeadingLevel2 } from '../../components/ResponsiveHeadinger';
+
+const Snarveier: React.FC = () => {
+  const { appEnv } = useApp();
+
+  return (
+    <VStack gap="2">
+      <HeadingLevel2 size="small" level="2">
+        Andre snarveier
+      </HeadingLevel2>
+      <HStack gap="4">
+        <LenkePanelMikro
+          tittel="Saksbehandlingstider"
+          headingLevel="3"
+          url={appEnv.saksbehandlingstiderUrl}
+        />
+        <LenkePanelMikro
+          tittel="Skriv til oss"
+          headingLevel="3"
+          url={appEnv.endringsmeldingUrl}
+        />
+      </HStack>
+    </VStack>
+  );
+};
+
+export default Snarveier;
