85dce131358113fc522a8fdc754732c12e512925
Viktor Solberg <viktor.grondalen.solberg@nav.no>
2024-01-23 13:40:05 +0100

    skal kunne vise dokumentoversikt for bruker

---
+++ src/pages/Dokumentoversikt/Dokument.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { IDokument } from './dummyData';
+import { FilePdfIcon } from '@navikt/aksel-icons';
+import styled from 'styled-components';
+import { ABorderDivider, AIconAction } from '@navikt/ds-tokens/dist/tokens';
+import { BodyShort, Detail, Label, VStack } from '@navikt/ds-react';
+
+interface Props {
+  dokument: IDokument;
+}
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 2rem 1fr;
+  column-gap: 1.25rem;
+  border-bottom: 1px solid ${ABorderDivider};
+  padding-top: 1.25rem;
+  padding-bottom: 1.25rem;
+
+  &:last-child {
+    border-bottom: none;
+  }
+`;
+
+const HovedDokument = styled(Label)`
+  text-decoration-line: underline;
+`;
+
+const Vedlegg = styled(BodyShort)`
+  text-decoration-line: underline;
+`;
+
+const Dokument: React.FC<Props> = ({ dokument }) => {
+  const detailTekst =
+    dokument.retning === 'I'
+      ? `Sendt inn av deg: ${dokument.opprettet}`
+      : `Sendt fra NAV: ${dokument.opprettet}`;
+
+  const harVedlegg = dokument.vedlegg.length > 0;
+
+  return (
+    <Container>
+      <FilePdfIcon
+        color={AIconAction}
+        title="a11y-title"
+        width="2.25rem"
+        height="2.25rem"
+      />
+      <VStack gap="5">
+        <div>
+          <HovedDokument>{dokument.tittel}</HovedDokument>
+          <Detail textColor="subtle">{detailTekst}</Detail>
+        </div>
+        {harVedlegg && (
+          <div>
+            <Detail weight="semibold" spacing={true}>
+              Vedlegg:
+            </Detail>
+            {dokument.vedlegg.map((vedlegg) => (
+              <Vedlegg spacing>{vedlegg.tittel}</Vedlegg>
+            ))}
+          </div>
+        )}
+      </VStack>
+    </Container>
+  );
+};
+
+export default Dokument;
--- src/pages/Dokumentoversikt/Dokumentoversikt.tsx
+++ src/pages/Dokumentoversikt/Dokumentoversikt.tsx
@@ -1,9 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
+import { contentWidthDesktop, contentWidthMobile, desktop } from '../../utils';
+import { dummyDokumenter } from './dummyData';
+import Dokument from './Dokument';
+import {
+  HeadingLevel1,
+  UnderTittel,
+} from '../../components/ResponsiveHeadinger';
+import { BodyShort, VStack } from '@navikt/ds-react';
+
+const Grid = styled.section`
+  display: grid;
+  grid-template-columns: minmax(auto, ${contentWidthMobile}px);
+  justify-content: center;
+
+  padding: 1rem 0.5rem;
+
+  @media (min-width: ${desktop}px) {
+    grid-template-columns: minmax(auto, ${contentWidthDesktop}px);
+    padding: 2rem 0.5rem;
+  }
+`;
 
 const DokumentOversikt: React.FC = () => {
+  const dokumenter = dummyDokumenter;
+
   return (
     <main id="maincontent" tabIndex={-1} role="main">
-      <p>Hello world</p>
+      <Grid>
+        <VStack gap="5">
+          <div>
+            <HeadingLevel1 size="medium" level="1">
+              Dokumentoversikt
+            </HeadingLevel1>
+            <UnderTittel>
+              Overgangsstønad, stønad til barnetilsyn og stønad til skolepenger
+            </UnderTittel>
+          </div>
+          <UnderTittel spacing>
+            Her finner du alle dine journalførte dokumenter relatert til stønad
+            til enslig mor eller far. Det kan ta noe tid fra du har sendt inn
+            dokumentene til de er synlige i oversikten.
+          </UnderTittel>
+        </VStack>
+        {dokumenter.map((dokument) => (
+          <Dokument dokument={dokument} />
+        ))}
+      </Grid>
     </main>
   );
 };
