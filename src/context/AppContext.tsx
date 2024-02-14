1b0f33b514bd6a88eba71a9da6941c6e2fba7f90
Viktor Solberg <viktor.grondalen.solberg@nav.no>
2024-02-14 09:06:45 +0100

    skal ikke lenger hente persondata for innlogget bruker

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,6 +1,5 @@
 import constate from 'constate';
 import { AppEnv } from '../api/env';
-import { useHentPersonData } from '../hooks/useHentPersonData';
 import { useEffect } from 'react';
 import { useHentJournalposter } from '../hooks/useHentJournalposter';
 import { useHentStønader } from '../hooks/useHentStønader';
@@ -10,17 +9,15 @@ interface Props {
 }
 
 const [AppProvider, useApp] = constate(({ appEnv }: Props) => {
-  const { hentPersonData, personData } = useHentPersonData();
   const { hentJournalposter, journalposter, journalpostStatus } = useHentJournalposter();
   const { hentStønader, stønader, stønadStatus } = useHentStønader();
 
   useEffect(() => {
-    hentPersonData();
     hentJournalposter();
     hentStønader();
-  }, [hentJournalposter, hentStønader, hentPersonData]);
+  }, [hentJournalposter, hentStønader]);
 
-  return { appEnv, journalposter, journalpostStatus, personData, stønader, stønadStatus };
+  return { appEnv, journalposter, journalpostStatus, stønader, stønadStatus };
 });
 
 export { AppProvider, useApp };
