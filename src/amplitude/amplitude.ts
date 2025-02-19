f3125b1b974c134ad95ce6b20e7f7f31c2cc2774
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2025-02-19 08:04:07 +0100

    Skal bruke Amplitude fra dekoratøren.

--- src/amplitude/amplitude.ts
+++ src/amplitude/amplitude.ts
@@ -1,20 +1,11 @@
-import * as amplitude from '@amplitude/analytics-browser';
+import { getAmplitudeInstance } from '@navikt/nav-dekoratoren-moduler';
 
-/*Default tracking is implicit tracking performed by Amplitude on your behalf, and includes page views, sessions, file downloads, form interactions, and marketing attribution.*/
+const logger = getAmplitudeInstance('dekoratoren');
 
-amplitude.init('default', undefined, {
-  serverUrl: 'https://amplitude.nav.no/collect-auto',
-  defaultTracking: false,
-  ingestionMetadata: {
-    sourceName: window.location.toString(),
-  },
-});
-
-// NAVs retningslinjer for amplitude taksonomi: https://github.com/navikt/analytics-taxonomy
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function logEvent(eventName: string, eventProperties: any) {
-  amplitude.track(eventName, eventProperties);
-}
+export const logEvent = (eventName: string, eventProperties: any) => {
+  logger(eventName, eventProperties);
+};
 
 export const logNavigering = (destinasjon: string, lenketekst: string, kilde?: string) => {
   logEvent('navigere', {
